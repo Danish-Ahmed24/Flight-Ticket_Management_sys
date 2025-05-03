073614335ba4547fb6f5e26116da6b2138d7b716
Danish-Ahmed24 <danishsp21@gmail.com>
2025-05-03 23:53:31 +0500

    Admin viewAllBookings done

--- src/ATS/Admin.java
+++ src/ATS/Admin.java
@@ -38,14 +38,13 @@ public class Admin {
 
             switch (option) {
                 case 1:
-                    System.out.println("Viewing Clients...");
-                    admin.viewClients();
+                    admin.viewClients();//view Client
                     break;
                 case 2:
-                    System.out.println("Viewing Planes...");
+                    admin.viewPlanes();//view Planes
                     break;
                 case 3:
-                    System.out.println("Viewing All Bookings...");
+                    admin.viewAllBookings();
                     break;
                 case 4:
                     System.out.println("Viewing All Flights...");
@@ -93,5 +92,52 @@ public class Admin {
             System.out.println(e.getMessage());
         }
     }
+    public void viewPlanes() {
+        try {
+            String query = "SELECT * FROM plane";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
 
+            //Process the ResultSet and display planes
+            while(resultSet.next())
+            {
+                int id = resultSet.getInt("id");
+                int admin_id = resultSet.getInt("admin_id");
+                String plane_model = resultSet.getString("plane_model");
+                String manufacturer = resultSet.getString("manufacturer");
+                int business_seats = resultSet.getInt("business_seats");
+                int economy_seats = resultSet.getInt("economy_seats");
+                System.out.println("ID: " + id + ", Admin ID: " + admin_id + ", Model: " + plane_model +
+                        ", Manufacturer: " + manufacturer + ", Business Seats: " + business_seats +
+                        ", Economy Seats: " + economy_seats);
+
+            }
+        }catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void viewAllBookings()
+    {
+        try{
+            String query = "SELECT * FROM booking";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            //Process the ResultSet and display planes
+            while (resultSet.next())
+            {
+                int id = resultSet.getInt("id");
+                int flight_id = resultSet.getInt("flight_id");
+                int client_id = resultSet.getInt("client_id");
+                boolean ispaid = resultSet.getBoolean("ispaid");
+                boolean isreserved = resultSet.getBoolean("isreserved");
+                float fees = resultSet.getFloat("fees");
+                System.out.println("ID: " + id + ", Flight ID: " + flight_id + ", Client ID: " + client_id + ", Is Paid: " + ispaid + ", Is Reserved: " + isreserved + ", Fees: $" + fees);
+            }
+        }catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
